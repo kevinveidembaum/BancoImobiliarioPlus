ac3ccbe7bc3e047936e0008fb5f9a29040b459e6
Kevin Nikolai <nikolai10052005@gmail.com>
2025-01-15 11:42:32 -0300

    feat: Jogador attributes and methods

--- src/Jogador.java
+++ src/Jogador.java
@@ -6,8 +6,59 @@ public class Jogador {
     private List<Emprestimo> emprestimosConcebidos;
     private List<Emprestimo> dividasEmprestimo;
 
+    public void comprarProprieda(Propriedade propriedade, Jogador dono){
 
+    }
 
+    public void venderPropriedade(Propriedade propriedade, Jogador comprador){
 
+    }
 
+    public void pagarAluguel(float valorAluguel, Jogador dono){
+
+    }
+
+    public void fazerEmprestimo(Jogador credor, float valorEmprestar, Propriedade garantia){
+
+    }
+
+    public void pagarEmprestimo(Emprestimo emprestimo, float valorPagar){
+
+    }
+
+    public void receberPagamentoEmprestimo(Emprestimo pagamento){
+
+    }
+
+    public float getDinheiro() {
+        return dinheiro;
+    }
+
+    public void setDinheiro(float dinheiro) {
+        this.dinheiro = dinheiro;
+    }
+
+    public List<Propriedade> getPropriedade() {
+        return propriedade;
+    }
+
+    public void setPropriedade(List<Propriedade> propriedade) {
+        this.propriedade = propriedade;
+    }
+
+    public List<Emprestimo> getDividasEmprestimo() {
+        return dividasEmprestimo;
+    }
+
+    public void setDividasEmprestimo(List<Emprestimo> dividasEmprestimo) {
+        this.dividasEmprestimo = dividasEmprestimo;
+    }
+
+    public List<Emprestimo> getEmprestimosConcebidos() {
+        return emprestimosConcebidos;
+    }
+
+    public void setEmprestimosConcebidos(List<Emprestimo> emprestimosConcebidos) {
+        this.emprestimosConcebidos = emprestimosConcebidos;
+    }
 }
