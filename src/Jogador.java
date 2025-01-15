507cabeebb03659e6f2e3a4776782755c4e262ec
Kevin Nikolai <nikolai10052005@gmail.com>
2025-01-15 11:22:28 -0300

    add: classes structure

---
+++ src/Jogador.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Jogador {
+    private float dinheiro;
+    private List<Propriedade> propriedade;
+    private List<Emprestimo> emprestimosConcebidos;
+    private List<Emprestimo> dividasEmprestimo;
+
+
+
+
+
+}
